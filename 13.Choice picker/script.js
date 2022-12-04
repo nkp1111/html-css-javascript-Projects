9b04b7b6360fc86a1469a446f58f974a4aa7933d
Neeraj Parmar <105532413+nkp1111@users.noreply.github.com>
2022-12-04 21:01:17 +0530

    Update script.js

--- 13.Choice picker/script.js
+++ 13.Choice picker/script.js
@@ -2,23 +2,18 @@
 const textarea = document.querySelector('textarea')
 const choice = document. querySelector('.choice-container')
 
-
-
 // focus on textarea after loading
 textarea.focus()
 
 // Creating a tag at keyup
 textarea.addEventListener('keyup', (e) => {
   getTag(e.target.value)  // key are taken into the function
-
   if (e.key === 'Enter') {
     setTimeout(() => {
       e.target.value = ""
     }, 10)
-
     pickATag()
   }
-
 })
 
 // function1 take value and create a tag from it
@@ -30,16 +25,13 @@ function getTag(item) {
   /* Since we have already created an array of the text inside `textarea` 
     We cleared it so that it will start new array-element separated by comma*/
 
-
   tags.forEach((tag) => {                           // Created a tag put it inside
     const tagEl = document.createElement('span')
     tagEl.innerText = tag
     tagEl.classList.add('tag')
     choice.appendChild(tagEl)
-
   })
 }
-
 
 function pickATag() {
   // Number of times the winner will pass
@@ -47,11 +39,9 @@ function pickATag() {
   const interval = setInterval(() => {
     const winner = selectRandom()
     highlight(winner)
-
     setTimeout(() => {
       unHighlight(winner)
     }, 100)
-
   },100)
 
   setTimeout(() => {
@@ -67,15 +57,12 @@ function selectRandom() {
   const tagEls = document.getElementsByClassName('tag')
   const randomIdx = Math.floor(Math.random() * tagEls.length)
   return tagEls[randomIdx]
-
 }
 
 function highlight(tag) {
   tag.classList.add('active')
-
 }
 
 function unHighlight(tag) {
   tag.classList.remove('active')
-
 }
